2316f32923415d446257c2ad240431b8fbca965d
Oleksandr Yakob <yakoboleksandr@gmail.com>
2021-12-17 15:41:07 +0200

    adjust files

--- js/script.js
+++ js/script.js
@@ -1,45 +1,10 @@
-// const atTheOldToad = {
-//   potions: [
-//     { name: 'Speed potion', price: 460 },
-//     { name: 'Dragon breath', price: 780 },
-//     { name: 'Stone skin', price: 520 },
-//   ],
-//   getPotions() {
-//     return this.potions;
-//   },
-//   addPotion(newPotion) {
-//     for (const potion of this.potions) {
-//       if (potion.name == newPotion.name) {
-//         return `Error! Potion ${newPotion.name} is already in your inventory!`;
-//       }
-//     }
-//     this.potions.push(newPotion);
-//   },
-//   removePotion(potionName) {
-//     for (const potion of this.potions) {
-//       if (potion.name === potionName) {
-//         return this.potions.splice(this.potions.indexOf(potion), 1);
-//       }
-//     }
-//     return `Potion ${potionName} is not in inventory!`;
-//   },
-//   updatePotionName(oldName, newName) {
-//     for (const potion of this.potions) {
-//       if (potion.name === oldName) {
-//         return (this.potions[this.potions.indexOf(potion)].name = newName);
-//       }
-//     }
-//     return `Potion ${oldName} is not in inventory!`;
-//   },
-// };
-
+'use strict';
 // const atTheOldToad = {
 //   potions: [
 //     { name: "Speed potion", price: 460 },
 //     { name: "Dragon breath", price: 780 },
 //     { name: "Stone skin", price: 520 },
 //   ],
-//   // Change code below this line
 //   getPotions() {
 //     const { potions } = this;
 //     return potions;
@@ -72,5 +37,85 @@
 //            }
 //            return this.potions;
 // },
-//   // Change code above this line
 // };
+// const users = [
+//   {
+//     name: 'Moore Hensley',
+//     email: 'moorehensley@indexia.com',
+//     eyeColor: 'blue',
+//     friends: ['Sharron Pace'],
+//     isActive: false,
+//     balance: 2811,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Sharlene Bush',
+//     email: 'sharlenebush@tubesys.com',
+//     eyeColor: 'blue',
+//     friends: ['Briana Decker', 'Sharron Pace'],
+//     isActive: true,
+//     balance: 3821,
+//     gender: 'female',
+//   },
+//   {
+//     name: 'Ross Vazquez',
+//     email: 'rossvazquez@xinware.com',
+//     eyeColor: 'green',
+//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//     isActive: false,
+//     balance: 3793,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Elma Head',
+//     email: 'elmahead@omatom.com',
+//     eyeColor: 'green',
+//     friends: ['Goldie Gentry', 'Aisha Tran'],
+//     isActive: true,
+//     balance: 2278,
+//     gender: 'female',
+//   },
+//   {
+//     name: 'Carey Barr',
+//     email: 'careybarr@nurali.com',
+//     eyeColor: 'blue',
+//     friends: ['Jordan Sampson', 'Eddie Strong', 'Adrian Cross'],
+//     isActive: true,
+//     balance: 3951,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Blackburn Dotson',
+//     email: 'blackburndotson@furnigeer.com',
+//     eyeColor: 'brown',
+//     friends: ['Jacklyn Lucas', 'Linda Chapman', 'Adrian Cross', 'Solomon Fokes'],
+//     isActive: false,
+//     balance: 1498,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Sheree Anthony',
+//     email: 'shereeanthony@kog.com',
+//     eyeColor: 'brown',
+//     friends: ['Goldie Gentry', 'Briana Decker'],
+//     isActive: true,
+//     balance: 2764,
+//     gender: 'female',
+//   },
+// ];
+
+// const getSortedFriends = users =>
+//   [...users]
+//     .filter((friend, index, array) => array.indexOf(friend) === index)
+//     .flatMap(user => user.friends)
+//     .sort((a, b) => a.friends.length.localeCompare(b.friends.length));
+// console.log(getSortedFriends());
+
+// const arr = [1, 2, 3, 4, 5];
+// const forEach = function (index = 0) {
+//   if (index >= this.length) return;
+//   console.log(this[index]);
+//   forEach.call(this, ++index);
+// };
+
+// forEach.call(arr);
